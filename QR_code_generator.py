e2a0d512453ce1ca7264b84cafc089d257df9f3a
Williams Odunayo <williamsodunayo70@gmail.com>
2025-01-23 19:36:10 +0100

    ...

--- QR_code_generator.py
+++ QR_code_generator.py
@@ -1,5 +1,8 @@
 # import library to use
 import qrcode as qr
+import numpy as np
+
+
 
 # an example
 data = "https://www.google.com/"
@@ -8,7 +11,28 @@ data = "https://www.google.com/"
 qr_filename = "google_qr.png"
 
 # generate qr code
-site_qr_img = qr.make(data)
+qr_img = qr.make(data)
 
 # save img to a file
-site_qr_img.save(qr_filename)
+qr_img.save(qr_filename)
+
+
+
+# recall data to be encoded
+data = "https://www.google.com/"
+
+# instantiate QRCode object
+qrcode = qr.QRCode(version=1, box_size=10, border=5)
+
+# add data to the QR code
+qrcode.add_data(data)
+
+# compile the data into a QR code array
+qrcode.make()
+
+# print the image shape
+print("The shape of the QR image:", np.array(qr.get_matrix()).shape)
+# transfer the array into an actual image
+img = qr.make_image(fill_color="white", back_color="black")
+# save it to a file
+img.save("site_inversed.png")
