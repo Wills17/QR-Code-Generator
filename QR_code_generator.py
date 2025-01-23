f2d96959713d9b836bf235894c22bbce0a75ef42
Williams Odunayo <williamsodunayo70@gmail.com>
2025-01-23 19:53:50 +0100

    ...

--- QR_code_generator.py
+++ QR_code_generator.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 
-# an example
+# an example to be encoded
 data = "https://www.google.com/"
 
 # output file name
@@ -16,10 +16,14 @@ img = qr.make(data)
 # save img to a file
 img.save(qr_filename)
 
+# display image produced
+img.show(qr_filename)
 
 
-# recall data to be encoded
-data = "https://www.google.com/"
+
+
+# new data to be encoded
+data = "https://www.github.com/"
 
 # create new QRCode object
 qrcode = qr.QRCode(version=1, box_size=10, border=5)
@@ -34,7 +38,9 @@ qrcode.make()
 print("The shape of the QR Code image is:", np.array(qrcode.get_matrix()).shape)
 
 # transfer the array into an actual image
-img = qrcode.make_image(fill_color="black", back_color="white")
+img = qrcode.make_image(fill_color="white", back_color="black")
 
 # save it to a file
 img.save("qr_image2.png")
+
+img.show()
