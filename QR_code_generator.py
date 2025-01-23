ca79774c9c00e20669ca87ee3984394fc79e2eae
Williams Odunayo <williamsodunayo70@gmail.com>
2025-01-23 20:54:05 +0100

    Updated codes

--- QR_code_generator.py
+++ QR_code_generator.py
@@ -13,7 +13,7 @@ qr_filename = "qr_image1.png"
 img = qr.make(data)
 
 # display code encoded
-img.show()
+#img.show()
 
 # save img to a file
 img.save(qr_filename)
@@ -36,25 +36,58 @@ qrcode.make()
 print("The shape of the QR Code image is:", np.array(qrcode.get_matrix()).shape)
 
 # transfer the array into an actual image
-img = qrcode.make_image(fill_color="black", back_color="yellow")
+img = qrcode.make_image(fill_color="black", back_color="white")
+
+# display new code encoded
+#img.show()
 
 # save it to a file
 img.save("qr_image2.png")
 
-# display new code encoded
-img.show()
+
 
 
 """Read QR Code"""
 
-import cv2
+import cv2 as cv
 
 # read a QRCODE image
-img = cv2.imread("qr.image2.png")
+img = cv.imread("qr_image2.png")
+
+if img is None:
+    print("Error: Image not found!")
+    exit()
+    
+# display original image
+cv.imshow("qr_image2.png", img)
 
 # initialize QRCode detector
-qr_detector = cv2.QRCodeDetector()
+qr_detector = cv.QRCodeDetector()
 
 # detect and decode the image
 data, bbox, straight_qrcode = qr_detector.detectAndDecode(img)
 
+# bounding box to know there is a QR code
+if bbox is not None:
+    print(f"QRCode data:\n{data}")
+    
+    print(f"Bounding Box:\n{bbox}")
+    
+    # display the image with lines
+    # length of bounding box
+    n_lines = len(bbox)
+    
+    for i in range(n_lines):
+        
+        # draw all lines        
+        point1 = tuple(map(int, bbox[i][0]))
+        point2 = tuple(map(int, bbox[(i + 1) % n_lines][0]))  # Connect to the next point (looping back)
+        
+        
+        cv.line(img, point1, point2, color=(255, 0, 0), thickness=2)
+        
+        
+# display the result
+cv.imshow("With bounding box", img)
+cv.waitKey(0)
+cv.destroyAllWindows()
